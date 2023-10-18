eaa663a338e530f942cc0a9426c2fc572cf4c52c
Toby Keech <toby.keech18@gmail.com>
2023-10-18 17:40:21 +0100

    bolier plate

---
+++ server/pokemonservice/src/main/java/com/toby/pokemon/pokemonservice/models/Pokemon.java
@@ -0,0 +1,22 @@
+package com.toby.pokemon.pokemonservice.models;
+
+public class Pokemon {
+
+    private String name;
+
+    private int hp;
+
+    private String type;
+
+    public Pokemon ( String name, int hp, String type){
+        this.name = name;
+        this.hp = hp;
+        this. type = type;
+    }
+
+
+
+
+
+
+}
