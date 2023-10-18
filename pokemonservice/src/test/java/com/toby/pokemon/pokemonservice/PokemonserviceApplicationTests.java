d29f75edea924e749817c2b6db1dea7d6d85e262
Toby Keech <toby.keech18@gmail.com>
2023-10-18 16:25:20 +0100

    adding files

---
+++ pokemonservice/src/test/java/com/toby/pokemon/pokemonservice/PokemonserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toby.pokemon.pokemonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
